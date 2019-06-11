897f32cfeec61dd0ec4ec9e40976dc7f865d6a68
Mark Heckler <mark.heckler@gmail.com>
2019-06-10 19:13:43 -0500

    Initial commit

---
+++ src/main/java/com/thehecklers/explorespringboot/ExploreSpringBootApplication.java
@@ -0,0 +1,94 @@
+package com.thehecklers.explorespringboot;
+
+import lombok.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class ExploreSpringBootApplication {
+    @Bean
+    CommandLineRunner saveDogs(DogRepo repo) {
+        return args -> {
+            Stream.of("Golden Retriever", "Cocker Spaniel", "Dachshund")
+                    .map(Dog::new)
+                    .forEach(repo::save);
+
+            repo.findAll()
+                    .forEach(System.out::println);
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreSpringBootApplication.class, args);
+    }
+
+}
+
+@RestController
+@RequestMapping("/dogs")
+class SimpleController {
+    private static final Logger logger = LoggerFactory.getLogger(ExploreSpringBootApplication.class);
+    private final DogRepo repo;
+
+    @Value("${server.port:8080}")
+    private int i;
+
+    SimpleController(DogRepo repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping("/hello")
+    String home() {
+        logger.info("WARN : Calling home() {}", i);
+        return "Hola mundo!";
+    }
+
+    @GetMapping
+    Iterable<Dog> getDogs() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Optional<Dog> getDogById(@PathVariable Long id) {
+        return repo.findById(id);
+    }
+
+    @PostMapping("/{type}")
+    Dog saveNewDog(@PathVariable String type) {
+        Dog existingDog = repo.findByType(type);
+        if (null == existingDog) {
+            repo.save(new Dog(type));
+        }
+
+        return repo.findByType(type);
+    }
+}
+
+interface DogRepo extends CrudRepository<Dog, Long> {
+    Dog findByType(String type);
+}
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+class Dog {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String type;
+}
---
+++ src/test/java/com/thehecklers/explorespringboot/ExploreSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thehecklers.explorespringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExploreSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
